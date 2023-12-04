57142dee8f95f05a96422056a8487cefac82581d
Reddy206 <78656384+Reddy206@users.noreply.github.com>
2023-12-04 16:26:45 +0530

    Add files via upload

---
+++ cab-book-driver/src/main/java/com/kafka/cabbookdriver/service/CabLocationService.java
@@ -0,0 +1,19 @@
+package com.kafka.cabbookdriver.service;
+
+import com.kafka.cabbookdriver.constants.AppConstants;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CabLocationService {
+
+    @Autowired
+    private KafkaTemplate<String,Object> kafkaTemplate;
+
+    public boolean updateLocation(String location){
+        kafkaTemplate.send(AppConstants.CAB_LOCATION,location);
+        return true;
+    }
+
+}
